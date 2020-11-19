c2db9fa6c32c78b5b691c5a457b48b374ad4fbc9
Al James <al@ometria.com>
2020-11-19 11:49:10 +0000

    Added extra message

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully! Yay!',
+        message: 'Go Serverless v1.0! Your function executed successfully! Yay! 2',
         input: event,
       },
       null,
